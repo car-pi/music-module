d3329b81b7da252f8e7bb010619448d88c8c4ba1
Jumner <justin462@protonmail.com>
2026-03-17 20:58:52 -0400

    I think I got music working and on start

--- install/common/common.sh
+++ install/common/common.sh
@@ -7,31 +7,39 @@ MODULE_NAME=$(basename "$MODULE_ROOT")   # infer module name from dir name
 MODULE_CLASS="common"
 MODULE_ID=$MODULE_NAME-$MODULE_CLASS
 SERVICE_SRC="$MODULE_ROOT/install/common/$MODULE_ID.service"
-SERVICE_DST="/etc/systemd/system/$MODULE_ID.service"
+SERVICE_DST="/usr/lib/systemd/user/$MODULE_ID.service"
 
-# # Install mpd and mpc
+# Install audio stack
+sudo apt install -y pipewire pipewire-alsa pipewire-pulse pipewire-bin pipewire-audio wireplumber libspa-0.2-modules
+
+# Install mpd and mpc
 
 sudo apt install -y mpd mpc
 
-# # Install ymuse
+# Copy config
+cp $MODULE_ROOT/config/common/mpd.conf ~/.config/mpd/mpd.conf
+
+# Install ymuse
 
 sudo apt install -y ./ymuse_0.22_linux_arm64.deb
 
-# # --- INSTALL SYSTEMD SERVICE ---
+# --- INSTALL SYSTEMD SERVICES ---
 echo "Installing systemd service: $SERVICE_DST"
 sudo cp "$SERVICE_SRC" "$SERVICE_DST"
 sudo chmod 644 "$SERVICE_DST"
 
-# # Reload systemd to pick up new/changed service file
+# Reload systemd to pick up new/changed service file
 echo "Reloading systemd daemon"
-sudo systemctl daemon-reload
+systemctl --user daemon-reload
 
-# # --- ENABLE & START SERVICE ---
-echo "Enabling $MODULE_ID.service"
-sudo systemctl enable "$MODULE_ID.service"
+# --- ENABLE & START SERVICE ---
+echo "Enabling Services"
+systemctl --user enable mpd.service
+systemctl --user enable "$MODULE_ID.service"
 
-echo "Starting $MODULE_ID.service"
-sudo systemctl restart "$MODULE_ID.service"
+echo "Starting Services"
+systemctl --user restart mpd.service
+systemctl --user restart "$MODULE_ID.service"
 
 echo "Deployment complete for module: $MODULE_ID"
 
